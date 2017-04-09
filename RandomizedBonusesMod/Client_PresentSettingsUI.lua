1d56c38ae0f305c7cf1382028c3579c77e3102b2
Fizzer <randyficker@gmail.com>
2017-04-08 18:55:54 -0700

    Fix settings panel error.  Fix non-overridden bonuses showing as overridden.

--- RandomizedBonusesMod/Client_PresentSettingsUI.lua
+++ RandomizedBonusesMod/Client_PresentSettingsUI.lua
@@ -3,6 +3,6 @@ function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	
 	UI.CreateLabel(vert).SetText('Random +/- limit of ' .. Mod.Settings.RandomizeAmount);
-	UI.CreateLabel(vert).SetText('Allow negative: ' .. Mod.Settings.AllowNegative);
+	UI.CreateLabel(vert).SetText('Allow negative: ' .. tostring(Mod.Settings.AllowNegative));
 end
 
--- RandomizedBonusesMod/Server_Created.lua
+++ RandomizedBonusesMod/Server_Created.lua
@@ -5,19 +5,23 @@ function Server_Created(game, settings)
     for _, bonus in pairs(game.Map.Bonuses) do
 		--skip negative bonuses unless AllowNegative was checked
 		if (bonus.Amount > 0 or Mod.Settings.AllowNegative) then 
+			local rndAmount = math.random(-Mod.Settings.RandomizeAmount, Mod.Settings.RandomizeAmount);
 
-			local newValue = bonus.Amount + math.random(-Mod.Settings.RandomizeAmount, Mod.Settings.RandomizeAmount);
+			if (rndAmount ~= 0) then --don't do anything if we're not changing the bonus.  We could leave this check off and it would work, but it show up in Settings as an overridden bonus when it's not.
 
-			-- don't take a positive or zero bonus negative unless AllowNegative was checked.
-			if (newValue < 0 and not Mod.Settings.AllowNegative) then
-				newValue = 0;
-			end
+				local newValue = bonus.Amount + rndAmount;
 
-			-- -1000 to +1000 is the maximum allowed range for overridden bonuses, never go beyond that
-			if (newValue < -1000) then newValue = -1000 end;
-			if (newValue > 1000) then newValue = 1000 end;
+				-- don't take a positive or zero bonus negative unless AllowNegative was checked.
+				if (newValue < 0 and not Mod.Settings.AllowNegative) then
+					newValue = 0;
+				end
+
+				-- -1000 to +1000 is the maximum allowed range for overridden bonuses, never go beyond that
+				if (newValue < -1000) then newValue = -1000 end;
+				if (newValue > 1000) then newValue = 1000 end;
 		
-			overriddenBonuses[bonus.ID] = newValue;
+				overriddenBonuses[bonus.ID] = newValue;
+			end
 		end
     end
 
