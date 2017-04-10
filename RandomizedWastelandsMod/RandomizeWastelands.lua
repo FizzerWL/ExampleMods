885c7b8317dd55b5ca51e4fcdea8272a0b05bbdd
Fizzer <randyficker@gmail.com>
2017-04-10 10:16:51 -0700

    Changes for Mods 0.03

--- RandomizedWastelandsMod/RandomizeWastelands.lua
+++ RandomizedWastelandsMod/RandomizeWastelands.lua
@@ -3,11 +3,11 @@
 function RandomizeWastelands(game, standing)
 	for _, territory in pairs(standing.Territories) do
         local numArmies = territory.NumArmies.NumArmies;
-        if (territory.OwnerPlayerID == PlayerID.Neutral and numArmies == game.Settings.WastelandSize) then
+        if (territory.OwnerPlayerID == WL.PlayerID.Neutral and numArmies == game.Settings.WastelandSize) then
             local newArmies = math.random(-Mod.Settings.RandomizeAmount, Mod.Settings.RandomizeAmount) + numArmies;
             if (newArmies < 0) then newArmies = 0 end;
             if (newArmies > 100000) then newArmies = 100000 end;
-            territory.NumArmies = Armies(newArmies);
+            territory.NumArmies = WL.Armies.Create(newArmies);
         end
     end
 end
