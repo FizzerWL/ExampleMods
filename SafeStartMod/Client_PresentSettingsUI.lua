e16d7c8a11c32b90fae90fe90093d8986f088a36
Fizzer <randyficker@gmail.com>
2017-04-15 12:55:50 -0700

    Added SafeStart mod

---
+++ SafeStartMod/Client_PresentSettingsUI.lua
@@ -0,0 +1,7 @@
+
+function Client_PresentSettingsUI(rootParent)
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	
+	UI.CreateLabel(vert).SetText('Cannot attack other players for the first ' .. Mod.Settings.NumTurns .. ' turns');
+end
+
---
+++ SafeStartMod/Client_SaveConfigureUI.lua
@@ -0,0 +1,5 @@
+
+function Client_SaveConfigureUI(alert)
+    
+    Mod.Settings.NumTurns = numberInputField.GetValue();
+end
