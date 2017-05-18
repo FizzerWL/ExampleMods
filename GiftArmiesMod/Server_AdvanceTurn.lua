aad5dc8052aa67a4d3a53e3bf88138c847da4c8f
Fizzer <randyficker@gmail.com>
2017-05-17 17:13:15 -0700

    Added new example mod, gift armies.

---
+++ GiftArmiesMod/Server_AdvanceTurn.lua
@@ -0,0 +1,44 @@
+require('Utilities');
+
+function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrder)
+    if (order.proxyType == 'GameOrderCustom') then  --look for the order that we inserted in Client_PresentMenuUI
+		--in Client_PresentMenuUI, we comma-delimited the number of armies, the target territory ID, and the target player ID.  Break it out here
+		local payloadSplit = split(order.Payload, ','); 
+		local numArmies = tonumber(payloadSplit[1])
+		local targetTerritoryID = tonumber(payloadSplit[2]);
+		local targetPlayerID = tonumber(payloadSplit[3]);
+
+		local armiesOnTerritory = game.ServerGame.LatestTurnStanding.Territories[targetTerritoryID].NumArmies.NumArmies;
+
+		if (numArmies < 0) then numArmies = 0 end;
+		if (numArmies > armiesOnTerritory) then numArmies = armiesOnTerritory end;
+
+		local terrMods = {};
+		--helper function
+		local modArmies = function(terrID, numArmies)
+			local existing = first(terrMods, function(m) return m.TerritoryID == terrID end);
+			if (existing ~= nil) then
+				existing.SetArmiesTo = existing.SetArmiesTo + numArmies;
+			else
+				local mod = WL.TerritoryModification.Create(terrID);
+				mod.SetArmiesTo = game.ServerGame.LatestTurnStanding.Territories[terrID].NumArmies.NumArmies + numArmies;
+				table.insert(terrMods, mod);
+			end
+		end
+		--First remove armies from the source territory
+		modArmies(targetTerritoryID, -numArmies);
+
+		--Now randomly distribute those armies on the target player's territories, one at a time.
+		local targetTerritories = map(filter(game.ServerGame.LatestTurnStanding.Territories, function(t) return t.OwnerPlayerID == targetPlayerID end), function(t) return t.ID; end);
+		for i=1,numArmies do
+			modArmies(randomFromArray(targetTerritories), 1);
+		end
+		
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, order.Message, {}, terrMods));
+		
+
+		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage); --we replaced the GameOrderCustom with a GameOrderEvent, so get rid of the custom order.  There wouldn't be any harm in leaving it there, but it adds clutter to the orders list so it's better to get rid of it.
+
+	end
+
+end
--- SafeStartMod/Server_AdvanceTurn.lua
+++ SafeStartMod/Server_AdvanceTurn.lua
@@ -4,12 +4,16 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 		print('NumTurns=' .. game.Game.NumberOfTurns .. ' Mod.Settings.NumTurns=' .. tostring(Mod.Settings.NumTurns) .. ' From=' .. game.Map.Territories[order.From].Name .. ' to=' .. game.Map.Territories[order.To].Name .. ' IsAttack=' .. tostring(result.IsAttack) .. ' DestinationOwner=' .. tostring(game.ServerGame.LatestTurnStanding.Territories[order.To].OwnerPlayerID));
 	end ]]--
 
+
+
     if (game.Game.NumberOfTurns < Mod.Settings.NumTurns  -- are we at the start of the game, within our defined range?  (without this check, we'd affect the entire game, not just the start)
 		and order.proxyType == 'GameOrderAttackTransfer'  --is this an attack/transfer order?  (without this check, we'd stop deployments or cards)
 		and result.IsAttack  --is it an attack? (without this check, transfers wouldn't be allowed within your own territory or to teammates)
 		and not IsDestinationNeutral(game, order)) then --is the destination owned by neutral? (without this check we'd stop people from attacking neutrals)
-		skipThisOrder(WL.ModOrderControl.Skip);
 
+		skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage); --skip it
+		local msg = 'Safe start mod skipped attack to ' .. game.Map.Territories[order.To].Name .. ' from ' .. game.Map.Territories[order.From].Name;
+		addNewOrder(WL.GameOrderEvent.Create(order.PlayerID, msg, {}, {}));
 	end
 
 end
