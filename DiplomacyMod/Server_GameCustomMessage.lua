b2b123b34fe82402b09cde7edae5ebea603e0425
Fizzer <randyficker@gmail.com>
2018-08-27 11:15:50 -0700

    Put parens around error so we can easily see if it's whitespace

--- DiplomacyMod/Server_GameCustomMessage.lua
+++ DiplomacyMod/Server_GameCustomMessage.lua
@@ -39,7 +39,7 @@ function Server_GameCustomMessage(game, playerID, payload, setReturnTable)
 			ProposalAccepted(proposal, game);
 		end
 	else
-		error("Payload message not understood: " .. payload.Message);
+		error("Payload message not understood (" .. payload.Message + ")");
 	end
 
 end
