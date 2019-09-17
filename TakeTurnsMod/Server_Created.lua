998e1ab37256b2bef459890f41d361c224113c5a
Fizzer <randyficker@gmail.com>
2019-09-17 15:01:58 -0700

    Give partial sanctions to avoid error

--- TakeTurnsMod/Server_Created.lua
+++ TakeTurnsMod/Server_Created.lua
@@ -3,8 +3,7 @@ require('Utilities');
 function Server_Created(game, settings)
 	--Turn on sanctions card.  Ensure its weight is 0 so players never get it.  This means that the sanctions card can't be used normally by the game creator.  https://www.warzone.com/wiki/Mod_API_Reference:CardGameSanctions
 	local cards = BuildMetatable(settings.Cards);
-	cards[WL.CardID.Sanctions] = WL.CardGameSanctions.Create(1, 0, 0, 0, 1, 1);
+	cards[WL.CardID.Sanctions] = WL.CardGameSanctions.Create(2, 0, 0, 1, 1, 1);
 	settings.Cards = cards;
-
 end
 
