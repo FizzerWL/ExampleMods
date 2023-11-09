e89de34e5c90b774e8f01ba6f35980faeca7a869
JustMe003 <kingoflol20033@gmail.com>
2023-11-09 21:36:22 +0100

    Update Client_PresentCommercePurchaseUI.lua

--- BuyNeutralMod/Client_PresentCommercePurchaseUI.lua
+++ BuyNeutralMod/Client_PresentCommercePurchaseUI.lua
@@ -1,6 +1,12 @@
 require('Client_PresentMenuUI');
 
 function Client_PresentCommercePurchaseUI(rootParent, game, close)
-	game.CreateDialog(Client_PresentMenuUI);
+	local vert = UI.CreateVerticalLayoutGroup(rootParent);
+	UI.CreateLabel(vert).SetText("You can try to purchase neutral territories for " .. Mod.Settings.CostPerNeutralArmy .. " per army on that territory").SetColor("#DDDDDD");
+	CreateButton(vert).SetText("Purchase territory").SetColor("#00FF05").SetOnClick(function()
+			-- Call Client_PresentMenuUI that will handle everything else
+			game.CreateDialog(Client_PresentMenuUI); 
+			close();
+		end);
 end
 
